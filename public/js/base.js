9edd4e7a72bbedc5c16f182e345cf86ddb4604dc
algking <alg_king@hotmail.com>
2016-04-10 12:59:38 +0800

    changes avatar,highlight

--- public/js/base.js
+++ public/js/base.js
@@ -47,8 +47,11 @@ function content_effects() {
       tag = current.prop('tagName').substr(-1);
       $("#nav").append("<div style='margin-left:" + 15 * (tag - 1) + "px'><a id='link" + i + "' href='#title" + i + "'>" + current.html() + "</a></div>");
     });
-    $("pre").addClass("prettyprint");
-    prettyPrint();
+    // $("pre").addClass("prettyprint");
+      // prettyPrint();
+
+      hljs.initHighlightingOnLoad();
+    
     $('#content img').addClass('img-thumbnail').parent('p').addClass('center');
     $('#content_btn').show();
   } else {
